87a336b85726b455f1e339ee142731d721b0c89e
PabloEscri <87900995+PabloEscri@users.noreply.github.com>
2023-02-16 16:04:44 +0100

    Ya funciona todo

--- server/controllers/inquilino.js
+++ server/controllers/inquilino.js
@@ -5,6 +5,7 @@ const uuid = require("uuid");
 const moment = require("moment");
 const fetch = require("node-fetch");
 const { response } = require("express");
+const Inmueble = require("../models/Inmueble");
 function getInquilinosActive(req, res) {
   const query = req.query;
 
@@ -20,12 +21,15 @@ function getInquilinosActive(req, res) {
 async function createInquilino(req, res) {
   const user = new Inquilino();
   try {
-    const { name, email, inmueble, telefono } = req.body;
+    const { name, email, inmueble, telefono, fecha_entrada, fecha_salida } =
+      req.body;
     user.name = name.toLowerCase();
     user.email = email.toLowerCase();
     user.inmueble = inmueble;
     user.telefono = telefono.toLowerCase();
     user.active = true;
+    user.fecha_salida = fecha_salida;
+    user.fecha_entrada = fecha_entrada;
 
     const code = uuid.v4().toString();
     user.code = code;
@@ -111,46 +115,64 @@ async function abrirPuertaInquilino(req, res) {
     }
     let code = userData.code;
     let encontrado = 0;
-    await Inquilino.find({ code: code }).then((inquilino) => {
-      inquilino = inquilino[0];
-      if (!inquilino) {
-        res
-          .status(404)
-          .send({ message: "No se ha encontrado ningun usuario." });
-      } else {
-        //Se ha encontrado un usuario
-        //Hay que comparar si la fecha de entrada y la de salida le permiten abrir la puerta
-
-        if (!moment(inquilino.fecha_entrada, "MM/DD/YYYY").isValid()) {
-          console.log("Error");
-        }
-        if (!moment(inquilino.fecha_salida, "MM/DD/YYYY").isValid()) {
-          console.log("Error");
-        }
-        var unixTimestamp = moment().unix();
-
-        console.log("Fecha entrada", inquilino.fecha_entrada);
-        console.log("HOY", moment.unix(unixTimestamp).format("MM/DD/YYYY"));
-        console.log("Fecha salida", inquilino.fecha_salida);
-        if (
-          moment().unix() >=
-            moment(inquilino.fecha_entrada, "MM/DD/YYYY").unix() &&
-          moment().unix() <= moment(inquilino.fecha_salida, "MM/DD/YYYY").unix()
-        ) {
-          console.log("Está entre las fechas");
-          encontrado = 1;
-          // res
-          //   .status(200)
-          //   .json({ "Abriendo puerta": "inquilino", Usuario: inquilino });
+    let my_inquilino = await Inquilino.find({ code: code }).then(
+      (inquilino) => {
+        inquilino = inquilino[0];
+        if (!inquilino) {
+          res
+            .status(404)
+            .send({ message: "No se ha encontrado ningun usuario." });
         } else {
-          console.log("No está entre las fechas");
-          res.status(400).json({ "Error ": "no autorizado" });
+          //Se ha encontrado un usuario
+          //Hay que comparar si la fecha de entrada y la de salida le permiten abrir la puerta
+
+          if (!moment(inquilino.fecha_entrada, "MM/DD/YYYY").isValid()) {
+            console.log("Error");
+          }
+          if (!moment(inquilino.fecha_salida, "MM/DD/YYYY").isValid()) {
+            console.log("Error");
+          }
+          var unixTimestamp = moment().unix();
+
+          console.log("Fecha entrada", inquilino.fecha_entrada);
+          console.log("HOY", moment.unix(unixTimestamp).format("MM/DD/YYYY"));
+          console.log("Fecha salida", inquilino.fecha_salida);
+          if (
+            moment().unix() >=
+              moment(inquilino.fecha_entrada, "MM/DD/YYYY").unix() &&
+            moment().unix() <=
+              moment(inquilino.fecha_salida, "MM/DD/YYYY").unix()
+          ) {
+            console.log("Está entre las fechas");
+            encontrado = 1;
+            return inquilino;
+            // res
+            //   .status(200)
+            //   .json({ "Abriendo puerta": "inquilino", Usuario: inquilino });
+          } else {
+            console.log("No está entre las fechas");
+            res.status(400).json({ "Error ": "no autorizado" });
+            return null;
+          }
         }
       }
-    });
+    );
     //TODO rescatarel id del shelly del piso en el que está el inquilino
 
     if (encontrado == 1) {
+      var inmueble = my_inquilino.inmueble;
+      console.log("El inquilino está en la vivienda: ", inmueble);
+      let shelly_id = await Inmueble.find({ _id: inmueble }).then(
+        (inmueble) => {
+          console.log("Encontrado ", inmueble);
+          console.log(
+            "turn=on&channel=0&id=" +
+              inmueble[0].shelly_abrir_puerta_ID +
+              "&auth_key=MTAyNDYwdWlk80C4102EA451A05640576257F285AF3199AADB08373424A1E7C477B557F58431BCE41679CD7DAF6F"
+          );
+          return inmueble[0].shelly_abrir_puerta_ID;
+        }
+      );
       var resp = await fetch(
         "https://shelly-38-eu.shelly.cloud/device/relay/control",
         {
@@ -158,7 +180,10 @@ async function abrirPuertaInquilino(req, res) {
           headers: {
             "Content-Type": "application/x-www-form-urlencoded",
           },
-          body: "turn=on&channel=0&id=349454741b10&auth_key=MTAyNDYwdWlk80C4102EA451A05640576257F285AF3199AADB08373424A1E7C477B557F58431BCE41679CD7DAF6F",
+          body:
+            "turn=on&channel=0&id=" +
+            shelly_id +
+            "&auth_key=MTAyNDYwdWlk80C4102EA451A05640576257F285AF3199AADB08373424A1E7C477B557F58431BCE41679CD7DAF6F",
         }
       ).then((response) => {
         return response.json();
@@ -170,7 +195,10 @@ async function abrirPuertaInquilino(req, res) {
             headers: {
               "Content-Type": "application/x-www-form-urlencoded",
             },
-            body: "turn=off&channel=0&id=349454741b10&auth_key=MTAyNDYwdWlk80C4102EA451A05640576257F285AF3199AADB08373424A1E7C477B557F58431BCE41679CD7DAF6F",
+            body:
+              "turn=off&channel=0&id=" +
+              shelly_id +
+              "&auth_key=MTAyNDYwdWlk80C4102EA451A05640576257F285AF3199AADB08373424A1E7C477B557F58431BCE41679CD7DAF6F",
           }).then((response) => {
             return response.json();
           });
